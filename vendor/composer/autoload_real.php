74c79cc36370925b3ab0751c012b870df4f41399
Markus Kalkbrenner <git@kalki.de>
2023-09-11 20:52:07 +0200

    drupal 10.1 upgrade, IO Board types and GPIO mapping

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -38,15 +38,15 @@ class ComposerAutoloaderInite0e6be606b7485a320a735325593597c
         $loader->register(true);
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInite0e6be606b7485a320a735325593597c::$files;
-        $requireFile = static function ($fileIdentifier, $file) {
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
             if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
                 $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
 
                 require $file;
             }
-        };
+        }, null, null);
         foreach ($filesToLoad as $fileIdentifier => $file) {
-            ($requireFile)($fileIdentifier, $file);
+            $requireFile($fileIdentifier, $file);
         }
 
         return $loader;
