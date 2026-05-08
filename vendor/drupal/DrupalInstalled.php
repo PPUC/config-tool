c02590cdd38ab4d294617a90d50b8ba7fe0a8dc5
Markus Kalkbrenner <git@kalki.de>
2026-05-08 16:12:29 +0200

    - Upgrading drupal/core (11.3.8 => 11.3.9)   - Upgrading drupal/core-composer-scaffold (11.3.8 => 11.3.9)   - Upgrading drupal/core-project-message (11.3.8 => 11.3.9)   - Upgrading roave/security-advisories (dev-latest 08cd07f => dev-latest da5ea38)   - Upgrading symfony/console (v7.4.8 => v7.4.9)   - Upgrading symfony/dependency-injection (v7.4.8 => v7.4.10)   - Upgrading symfony/deprecation-contracts (v3.6.0 => v3.7.0)   - Upgrading symfony/event-dispatcher (v7.4.8 => v7.4.9)   - Upgrading symfony/event-dispatcher-contracts (v3.6.0 => v3.7.0)   - Upgrading symfony/filesystem (v7.4.8 => v7.4.9)   - Upgrading symfony/http-kernel (v7.4.8 => v7.4.10)   - Upgrading symfony/mime (v7.4.8 => v7.4.9)   - Upgrading symfony/routing (v7.4.8 => v7.4.9)   - Upgrading symfony/serializer (v7.4.8 => v7.4.10)   - Upgrading symfony/service-contracts (v3.6.1 => v3.7.0)   - Upgrading symfony/translation-contracts (v3.6.1 => v3.7.0)   - Upgrading symfony/validator (v7.4.8 => v7.4.10)   - Upgrading symfony/var-exporter (v7.4.8 => v7.4.9)   - Upgrading symfony/yaml (v7.4.8 => v7.4.10)

--- vendor/drupal/DrupalInstalled.php
+++ vendor/drupal/DrupalInstalled.php
@@ -16,6 +16,6 @@ class DrupalInstalled {
   /**
    * A hash of all the installed packages and their versions.
    */
-  public const string VERSIONS_HASH = 'ce277e9150945e49';
+  public const string VERSIONS_HASH = '6996db88b73acf42';
 
 }
