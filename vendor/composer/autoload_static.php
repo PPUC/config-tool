5a20b2fa773a52c556ea8dc444aa249c02f1bfc0
Markus Kalkbrenner <git@kalki.de>
2026-09-19 12:06:11 +0200

    - Upgrading drupal/core (11.4.5 => 11.4.7)   - Upgrading drupal/core-composer-scaffold (11.4.5 => 11.4.7)   - Upgrading drupal/core-project-message (11.4.5 => 11.4.7)   - Upgrading drupal/views_bulk_operations (4.4.7 => 4.4.8)   - Upgrading drush/drush (13.7.6 => 13.8.0)   - Upgrading guzzlehttp/guzzle (7.15.3 => 7.15.5)   - Upgrading guzzlehttp/promises (2.5.2 => 2.5.3)   - Upgrading guzzlehttp/psr7 (2.13.0 => 2.13.1)   - Upgrading justinrainbow/json-schema (6.10.0 => 6.12.0)   - Upgrading laravel/prompts (v0.3.22 => v0.3.24)   - Upgrading masterminds/html5 (2.10.1 => 2.11.0)   - Upgrading mck89/peast (v1.17.7 => v1.18.1)   - Upgrading nikic/php-parser (v5.8.0 => v5.9.0)   - Upgrading npm-asset/nwsapi (2.2.24 => 2.2.27)   - Upgrading npm-asset/ws (8.21.2 => 8.21.3)   - Upgrading pear/archive_tar (1.6.0 => 1.6.1)   - Upgrading roave/security-advisories (dev-latest f4f38b8 => dev-latest f1dd47d)   - Upgrading sebastian/diff (7.0.0 => 7.0.1)   - Upgrading symfony/console (v7.4.16 => v7.4.19)   - Upgrading symfony/dependency-injection (v7.4.16 => v7.4.17)   - Upgrading symfony/error-handler (v7.4.15 => v7.4.17)   - Upgrading symfony/event-dispatcher (v7.4.15 => v7.4.17)   - Upgrading symfony/filesystem (v7.4.15 => v7.4.18)   - Upgrading symfony/finder (v7.4.14 => v7.4.19)   - Upgrading symfony/http-foundation (v7.4.16 => v7.4.19)   - Upgrading symfony/http-kernel (v7.4.16 => v7.4.19)   - Upgrading symfony/mailer (v7.4.15 => v7.4.19)   - Upgrading symfony/mime (v7.4.16 => v7.4.19)   - Upgrading symfony/polyfill-intl-idn (v1.38.1 => v1.42.0)   - Upgrading symfony/polyfill-intl-normalizer (v1.38.0 => v1.42.0)   - Upgrading symfony/process (v7.4.13 => v7.4.19)   - Upgrading symfony/routing (v7.4.15 => v7.4.18)   - Upgrading symfony/serializer (v7.4.16 => v7.4.19)   - Upgrading symfony/service-contracts (v3.7.1 => v3.7.3)   - Upgrading symfony/string (v7.4.15 => v7.4.19)   - Upgrading symfony/validator (v7.4.16 => v7.4.19)   - Upgrading symfony/var-dumper (v7.4.15 => v7.4.18)   - Upgrading symfony/var-exporter (v7.4.16 => v7.4.18)   - Upgrading symfony/yaml (v7.4.15 => v7.4.18)   - Upgrading twig/html-extra (v3.28.0 => v3.29.0)   - Upgrading twig/twig (v3.28.0 => v3.29.0)

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -594,7 +594,6 @@ class ComposerStaticInite0e6be606b7485a320a735325593597c
         'SebastianBergmann\\Diff\\Diff' => __DIR__ . '/..' . '/sebastian/diff/src/Diff.php',
         'SebastianBergmann\\Diff\\Differ' => __DIR__ . '/..' . '/sebastian/diff/src/Differ.php',
         'SebastianBergmann\\Diff\\Exception' => __DIR__ . '/..' . '/sebastian/diff/src/Exception/Exception.php',
-        'SebastianBergmann\\Diff\\InvalidArgumentException' => __DIR__ . '/..' . '/sebastian/diff/src/Exception/InvalidArgumentException.php',
         'SebastianBergmann\\Diff\\Line' => __DIR__ . '/..' . '/sebastian/diff/src/Line.php',
         'SebastianBergmann\\Diff\\LongestCommonSubsequenceCalculator' => __DIR__ . '/..' . '/sebastian/diff/src/LongestCommonSubsequenceCalculator.php',
         'SebastianBergmann\\Diff\\MemoryEfficientLongestCommonSubsequenceCalculator' => __DIR__ . '/..' . '/sebastian/diff/src/MemoryEfficientLongestCommonSubsequenceCalculator.php',
--- vendor/drupal/DrupalInstalled.php
+++ vendor/drupal/DrupalInstalled.php
@@ -16,6 +16,6 @@ class DrupalInstalled {
   /**
    * A hash of all the installed packages and their versions.
    */
-  public const string VERSIONS_HASH = '5a8f06b611978a01';
+  public const string VERSIONS_HASH = 'f4292a33f411ac1c';
 
 }
